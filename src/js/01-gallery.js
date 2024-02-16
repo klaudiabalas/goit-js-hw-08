5456ab999291aba254528babf0a8b0cdccc9110a
klaudiab <klaudiabalas1303@gmail.com>
2024-02-16 10:47:57 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Dodatkowy import stylów
-import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { galleryItems } from './gallery-items.js';
